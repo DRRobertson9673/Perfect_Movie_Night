4d36cf4e81150d37392db60578497992b91b0891
CcMong <fullstackcodex@gmail.com>
2023-02-03 19:27:27 +0000

    Updated the CSS, altered the JS for toggle switch and added code for the genre section.

--- js/script.js
+++ js/script.js
@@ -15,31 +15,33 @@ $.ajax(settings).done(function (response) {
 var userCountry = {
     netflix: ["ar","at","au","be","br","ca","ch","cl","co","cz","de","dk","ec","ee","es","fi","fr","gb","gr","hu","id","ie","in","it","jp","kr","lt","lv","mx","my","nl","no","nz","pe","ph","pl","pt","ro","ru","se","sg","th","tr","us","ve","za"],
 };
-var userService;
-var userType;
-var userGenre = [];
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": `https://streaming-availability.p.rapidapi.com/search/basic?country=${userCountry}&service=${userService}&type=${userType}&genre=${userGenre}&page=1&output_language=en&language=en`,
+var userCountry = "gb";
+var userService = "";
+var userType = "movie";
+var userGenre = "";
+
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": `https://streaming-availability.p.rapidapi.com/search/basic?country=${userCountry}&service=${userService}&type=${userType}&genre=${userGenre}&page=1&output_language=en&language=en`,
 	
-    "method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": apiKey.Charles,
-		"X-RapidAPI-Host": "streaming-availability.p.rapidapi.com"
-	}
-};
+//     "method": "GET",
+// 	"headers": {
+// 		"X-RapidAPI-Key": apiKey.Charles,
+// 		"X-RapidAPI-Host": "streaming-availability.p.rapidapi.com"
+// 	}
+// };
 
-$.ajax(settings).done(function (response) {
-	//console.log(response);
-	console.log(JSON.parse(response));
+// $.ajax(settings).done(function (response) {
+// 	//console.log(response);
+// 	console.log(JSON.parse(response));
 
-	var parsedResponse = JSON.parse(response);
-	console.log(parsedResponse.results[0].age);
+// 	var parsedResponse = JSON.parse(response);
+// 	console.log(parsedResponse.results[0].age);
 
-	//console.log(JSON.stringify(parsedResponse));​
-});
+// 	//console.log(JSON.stringify(parsedResponse));​
+// });
 //MOVIE TITLE
 //POSTER
 //TRAILER
@@ -47,6 +49,55 @@ $.ajax(settings).done(function (response) {
 //IMDB NUMBER TO GET THE OMDB STUFF
 //GENRE
 //
+
+/* 1. Listener for the service buttons - TESTED AND WORKS!!! NEXT STEP IS TO MAKE THEM DO WHAT WE WANT THEM TO.
+--------------------------------------*/
+
+$(document).on("click", ".btn", function(event) {
+	event.preventDefault();
+	userService = ($(this).attr("id")).toLowerCase(); 
+
+	console.log(userService); 
+
+	 
+});
+
+/* 2. Listener for type
+------------------------ */
+
+$("#TVSwitch").on("change", function() {
+
+	if(this.checked) {
+		// Get id, and populte userType variable
+		userType = "series";
+	} 
+
+	if(!this.checked) {
+
+		userType = "movie";
+
+	}
+
+	console.log(userType);
+	//console.log("my cat");
+	//console.log(this.checked);
+});
+
+console.log(userType);
+
+/* 3. Listener for Genre
+-------------------------*/
+
+$(document).on("click", ".genre-class", function() {
+
+	userGenre = $(this).attr("[data-genre]");
+
+	console.log(userGenre);
+})
+
+
+
+
 
 
 
