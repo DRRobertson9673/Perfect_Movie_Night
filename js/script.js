a818c53bb6101ef4c59e6dad31766255d869a241
CcMong <fullstackcodex@gmail.com>
2023-02-06 01:28:55 +0000

    Added the html for the watchlist poster rendering and the modals. Made a start on the JS code for the watchlist.

--- js/script.js
+++ js/script.js
@@ -151,3 +151,156 @@ $(".card").click(function () {
 /* Add to-, Remove from-, and Clear Watchlist Functionality
 ------------------------------------------------------------*/
 
+$("#back-search").on("click", function() {
+
+	window.location.href = "index.html";
+});
+
+// There are four events we will be looking at:
+
+//1. Clicking to add a movie or TV show to watchlist (if there is time, maybe create two different arrays for TV and Movies, and render them separately on the page);
+//2. Clicking to view watchlist
+//3. Clicking to remove a movie poster from the watchlist
+//4. Clicking to clear the watchlist
+
+
+/*1. Add Movie to Watchlist
+---------------------------*/
+
+// If the user chooses to click "Add to Watchlist" or "+" on a particular poster, we want two things to happen: 
+//1. That button turns to a tick (or something that indicates it is in the watchlist, without using a prompt)
+//2. All the information required to render that particular poster and its associated modal to be extracted from the response, put into an object, and then for that object to be pushed into an array (userWatchlist). 
+
+// The idea, every time the watchlist is loaded, is to take this userWatchlist array and use it to populate the watchlist page by rendering the respective posters and their attached modals (THIS WILL COME IN THE SECOND EVENT LATER)
+
+// $(".add-watchlist").on("click", function(event) {
+
+// 	event.preventDefault();
+
+// // Start by calling whatever is in local storage
+
+// 	var localWatchlist = localStorage.getItem("user-watchlist"); 
+
+// // Also define a couple of variables
+
+// 	var parsedWatchlist = JSON.parse(localWatchlist);
+
+// 	var userWatchlist;
+
+// // If there is nothing in local storage, then we set userWatchlist to an empty array.
+
+// 	if(!parsedWatchlist) {
+
+// 		userWatchlist = [];
+// 	} else {
+
+// // If there is something, though, then we want to parse whatever is in local storage, and set userWatchlist array to that.
+
+// 		userWatchlist = parsedWatchlist;
+
+// 	};
+
+// // Now, whenever the search results come up on the main page, we want to have the posters dynamically render from the response, along with an "add to watchlist" button which has a dataset attribute - call it data-index, for example. We want to set this to the index position of the movie within the response array. (This data-index = response.results[i] will come in handy when trying to extract the info required to populate the object used to render the watchlist posters and modals)
+
+// // We will first put the bits of information needed for the posters and the modals in variables eg:
+
+// 	var movieDatasetAttribute = $("#search-poster").attr("data-index");
+
+// 	var movieTitle = response.results[movieDatasetAttribute].title;
+// 	var moviePoster = response.results[movieDatasetAttribute].posterURLs[185];
+
+// etc etc
+
+// // And then create an object with them
+
+// 	var newMovieObject = {
+// 		title: movieTitle,
+// 		poster: moviePoster,
+// etc etc
+// 	}
+
+// // Next, push this new object into the userWatchlist array
+
+// 	userWatchlist.push(newMovieObject);
+
+// // THEN we want the userWatchlist array to be sorted in ascending alphabetical order based on the movie title
+// // https://stackoverflow.com/questions/8837454/sort-array-of-objects-by-single-key-with-date-value/70319634?noredirect=1#comment124367399_70319634
+// // https://www.scaler.com/topics/javascript-sort-an-array-of-objects/
+
+// // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// (NB: We can either sort the array here before the watchlist is rendered, OR add the movie as a dataset/id and sort the div elements after they've been rendered.)
+
+// // Once it is sorted, then it should be stringified and set in local storage. 
+
+// 	localStorage.setItem("user-watchlist", JSON.stringify(userWatchlist));
+
+// // Now, we have all the information required to render the user's preferred movie/show stored in local storage.
+
+// /*2. Viewing the Watchlist
+// --------------------------*/
+
+// // Whenever the user clicks "My Watchlist", then it should direct the user to the watchlist page.
+
+// $(".watchlist").on("click", function() {
+
+// 	window.location.href = "watchlist.html";
+
+// // We then call the information in local storage whenever the page is loaded and use this to populate the watchlist webpage
+
+// //(I think I might put this into a function "renderWatchlistPosters()", and then call the function at the end of the code base so that it triggers whenever the application runs. THEN the watchlist click will simply take the user to the watchlist BUT the watchlist will always be populated without being triggered by the click event.)
+	
+// 	var watchlistRenders = $("#xxx")
+
+// 	$(watchlistRenders).empty();
+
+// 	var localWatchlist = localStorage.getItem("user-watchlist");
+
+// 	var parsedWatchlist = JSON.parse(localWatchlist);
+
+// 	if(localWatchlist !== null) {
+
+// 		for (var i = 0; i < parsedWatchlist.length; i++) {
+
+// 			$(watchlistRenders).append(`WRITE IN HTML CODE TEMPLATE WITH parsedWatchlist[i] REFERENCES IN THE DATASET ATTRIBUTE, AND DIRECTLY REFERENCE THE PARSED OBJECT. IF THE OBJECT WAS NOT SORTED BEFORE, ADD MOVIE AS ID, AND WRITE AND CALL A FUNCTION TO SORT AT THIS STAGE BASED ON THIS IN ALPHABETICAL ORDER`);
+
+// 		} else {
+// 			$("<p>").text("Nothing added to your watchlist yet")
+// });
+
+
+// /*3. Remove from Watchlist
+// --------------------------*/
+
+// $("#remove-watchlist").on("click", function() {
+
+// 	var listIndex = $(this).attr("data-index");
+
+// 	var localWatchlist = localStorage.getItem("user-watchlist");
+
+// 	var parsedWatchlist = JSON.parse(localWatchlist);
+
+// 	userWatchlist = parsedWatchlist
+
+// 	var deletedWatchlistMovie = userWatchlist.splice(listIndex, 1);
+
+// 	localStorage.setItem("user-watchlist, JSON.stringify(userWatchlist));	
+
+// 	renderWatchlistPosters();
+// });
+
+
+// /*4. Clear the Watchlist
+// ------------------------*/
+
+// $("#clear-watchlist").on("click", function() {
+
+// 	localStorage.clear();
+
+// 	// Then either:
+// 	$(watchlistRenders).empty();
+
+// 	// OR simply refresh the page
+// 	location.reload();
+
+// });
